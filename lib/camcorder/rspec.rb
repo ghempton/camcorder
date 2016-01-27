dcd8054d59acc5195ba5a77c9bcd4dae35f03346
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2016-01-27 10:25:37 -0300

    Fixes the deprecation warning generated by Rspec. This code comes from https://github.com/vcr/vcr/blob/033a82473e42d1433c35c757abf1aaa739b817a5 /lib/vcr/test_frameworks/rspec.rb#L11-L26

--- lib/camcorder/rspec.rb
+++ lib/camcorder/rspec.rb
@@ -2,17 +2,27 @@
 # Setup the camcorder before every test
 #
 RSpec.configure do |config|
-  
-  recording_name_for = lambda do |metadata|
-    description = metadata[:description]
 
-    if example_group = metadata[:example_group]
-      [recording_name_for[example_group], description].reject{|d| !d || d == ''}.join('/')
+  recording_name_for = lambda do |metadata|
+    description = if metadata[:description].empty?
+                    # we have an "it { is_expected.to be something }" block
+                    metadata[:scoped_id]
+                  else
+                    metadata[:description]
+                  end
+    example_group = if metadata.key?(:example_group)
+                      metadata[:example_group]
+                    else
+                      metadata[:parent_example_group]
+                    end
+
+    if example_group
+      [recording_name_for[example_group], description].join('/')
     else
       description
     end
   end
-  
+
   config.around(:each) do |example|
     name = recording_name_for[example.metadata].gsub(/[^\w\-\/]+/, '_')
     filename = File.join(Camcorder.config.recordings_dir, "#{name}.yml")
